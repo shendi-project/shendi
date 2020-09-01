6694596e35a291640793473742f953247b9e3092
Auca Coyan <forospaginas@gmail.com>
2020-09-01 18:56:48 -0300

    Modification of the general UI. When you click OK creates a new tab with the PC name. Line 67: idk why but "lambda" fix it

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import webbrowser
 
 root = tk.Tk()
 root.title('shendi - Pathfinder 2nd Edition Character Builder')
-root.geometry("400x400")
 
+# Tabs
+my_notebook = ttk.Notebook()
 
-# Create a PC
-def create_pc(pc_name):
-    print(pc_name)
+my_frame1 = tk.Frame(my_notebook, width=500, height=500, bg="blue")
+my_frame1.pack(fill="both", expand=1)
 
 
 #       Menu Functions
@@ -25,6 +25,8 @@ def show_about():
 def go_to_github():
     webbrowser.open_new("https://github.com/shendi-project/shendi")
 
+
+# todo: separar menu en otro archivo
 
 #       Creating menu bar
 my_menu = tk.Menu(root)
@@ -49,27 +51,30 @@ enter_name_for_pc = tk.Label(root,
                              text="Enter the name of your character:")
 enter_name_for_pc.grid(row=0, column=0)
 
+
+# Create a PC
+def create_pc():
+    TabName = ttk.Frame(my_notebook)
+    getentry = pc_name_field.get()
+    my_notebook.add(my_frame1, text=getentry)
+
+
 # Creating an input field
 pc_name_field = tk.Entry(root,
                          width=30)
 
 # Creating the ok button
-ok_name = tk.Button(root, text="OK", command=create_pc(enter_name_for_pc.pack()))
+ok_name = tk.Button(root, text="OK", command=lambda: create_pc())
 
 # Locations of fields in the grid
 enter_name_for_pc.grid(row=0, column=0)
 pc_name_field.grid(row=0, column=1)
-ok_name.grid(row=0, column=3)
+ok_name.grid(row=0, column=2)
 
-
-# Tabs
-my_notebook = ttk.Notebook()
 # bottom limit (where the status bar goes)
 # my_notebook.pack(pady=15)
 
-my_frame1 = tk.Frame(my_notebook, width=500, height=500, bg="blue")
-my_frame1.pack(fill="both", expand=1)
-
 my_notebook.add(my_frame1, text="PC 1")
+my_notebook.grid(row=1, column=0, columnspan=3)
 
 root.mainloop()
