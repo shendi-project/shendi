160162418a2c3636c769a5ebedfa8d4c90ea6f38
Auca Coyan <forospaginas@gmail.com>
2020-09-09 22:34:13 -0300

    added JSON files for 2 ancestries, and modified the main.py to correctly import all the loaded anccestries of the JSON file

--- JSONancestries.py
+++ JSONancestries.py
@@ -9,5 +9,15 @@ list_of_races = []
 for index, race in enumerate(data):
     list_of_races.append(data[index]['race'])
 
+# for race in data:
+#     print(da)
 
-print(list_of_races)
+def main():
+    print(list_of_races)
+    print(list_of_races[0])
+    print(list_of_races[1])
+
+if __name__ == '__main__':
+    main()
+
+
